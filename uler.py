e6aab8bb9217bbece75695829c1c25f2ad03ce99
Christopher Chandra <nulvonoid@gmail.com>
2020-04-20 02:09:24 +0700

    kepala ulernya sekarang bia nambah panjang badannya pas abis emam

--- uler.py
+++ uler.py
@@ -30,6 +30,8 @@ emam.color("green")
 emam.penup()                  
 emam.goto(0,100)
 
+segmen = []       #tiap kali kepala emam, kelapa nambah segment.
+
 
 
 #function
@@ -75,11 +77,31 @@ layar.onkeypress(go_right, "Right")
 while True:
     layar.update()          #ini loop buat selalu update layar
 
-    if kepala.distance (emam) <20:
+    if kepala.distance (emam) <20:              #ini artinya collide, default ukuran turtle 20x20. jadi kalo jarak udah dibawah 20, berarti udah ketemu (kemakan)
         #pindahin emam random
         x = random.randint(-290, 290)
         y = random.randint(-290, 290)
         emam.goto(x,y)
+
+        #buat nambahin segment
+        segmen_baru = turtle.Turtle()
+        segmen_baru.speed(0)
+        segmen_baru.shape("square")
+        segmen_baru.color("grey")
+        segmen_baru.penup()
+        segmen.append(segmen_baru)      #ini nambah segmen_baru ke list segmen di atas
+
+    #pindahin segmen terakhir ke pertama tapi urutannya kebalik
+    for index in range(len(segmen)-1,0,-1):
+        x = segmen[index-1].xcor()
+        y = segmen[index-1].ycor()
+        segmen[index].goto(x,y)
+
+    #pindahin segmen ke - 0 ke tempat kepala
+    if len(segmen) > 0:
+        x = kepala.xcor()
+        y = kepala.ycor()
+        segmen[0].goto(x,y)
 
     gerak()
 
