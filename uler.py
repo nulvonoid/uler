99ec3fcdf8f9519345a07c159d1b278ea577bb9f
nulvonoid <nulvonoid@gmail.com>
2020-04-20 00:42:42 +0700

    update bos

--- uler.py
+++ uler.py
@@ -1,1 +1,14 @@
-print ('ini uler')
+#bikin uler-uleran https://www.youtube.com/watch?v=BP7KMlbvtOo
+
+import turtle               #install dulu, apt install python3-tk 
+
+layar = turtle.Screen()
+layar.title("Game Uler")
+layar.bgcolor("black")
+layar.setup(width = 600, height = 600)
+layar.tracer(0)
+
+
+
+
+layar.mainloop()            #ini baris terakhir
